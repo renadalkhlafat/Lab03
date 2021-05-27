90aee731228e126d2f44f27f7ae0b5cc9df566e7
RenadAlKhlafat <>
2021-05-27 12:10:26 +0000

    add javascript featuers to this website

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ if (r == true) {
   alert ( "This is Amazing ^-^");
 } else {
   alert("Here is 5 Ways To Be An Optimist During  Life Challenges");
-  var msg = prompt("Confirmation Message !","Are you ready to be Optimist During  Life Challenges ")
-}
+  var msg = prompt("Are you ready to be Optimist During  Life Challenges ")
+  if (msg == true) {
+  alert ( "This is Amazing ^-^");
+} else {
+  alert("Please ,Try to read ");
+}}
 

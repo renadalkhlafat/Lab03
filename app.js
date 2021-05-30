b357c3bdcadca4ce17d6ab0261e00ed0b2c697cc
RenadAlKhlafat <>
2021-05-30 12:06:51 +0000

    add some features to the website

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
 alert("Welcome to my website")
+// var age =prompt("please enter your age");
+// if (age >=0 || age)
 
-var txt;
-var r = confirm("Are  you feel optimistic");
-if (r == true) {
-  alert ( "This is Amazing ^-^");
-} else {
-  alert("Here is 5 Ways To Be An Optimist During  Life Challenges");
-  var msg = prompt("Are you ready to be Optimist During  Life Challenges ")
-  if (msg == true) {
-  alert ( "This is Amazing ^-^");
-} else {
-  alert("Please ,Try to read ");
-}}
+// var txt;
+var r = prompt("Are  you feel optimistic ,please type either yes or no ");
 
+while (r!="yes" || r!="no")
+{if (r == 'yes')
+ {
+  alert ( "This is Amazing ^-^");break;}
+ else if(r =='no') {
+  alert("Here is 5 Ways To Be An Optimist During  Life Challenges");break;}
+  else {
+    r= prompt ('please type either yes or no  ' )
+  }}
+ 
+
+var rate =prompt ('how many star rating would you give this content, 1-5?');
+
+for(var i=0;i<rate;i++)
+{ document.write('<img src="https://www.jing.fm/clipimg/detail/1-14150_your-a-star-star-icon-png.png" width="30" height="30"/>')}

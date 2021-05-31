c6251018192f38c926f7ac964e898de41845618f
RenadAlKhlafat <>
2021-05-31 11:30:35 +0000

    add css feature to this website

--- app.js
+++ app.js
@@ -1,22 +1,30 @@
 alert("Welcome to my website")
-// var age =prompt("please enter your age");
-// if (age >=0 || age)
+// checkState ,function to check the user either feel optimistic or not 
 
-// var txt;
-var r = prompt("Are  you feel optimistic ,please type either yes or no ");
-
-while (r!="yes" || r!="no")
-{if (r == 'yes')
+function checkState (state){ //the begining of function body 
+while (state!="yes" || state!="no")
+{if (state == 'yes')
  {
   alert ( "This is Amazing ^-^");break;}
- else if(r =='no') {
+ else if(state =='no') {
   alert("Here is 5 Ways To Be An Optimist During  Life Challenges");break;}
   else {
-    r= prompt ('please type either yes or no  ' )
-  }}
+    state= prompt ('please type either yes or no  ' )
+  }
+  }
+  }//the end of function body 
+
+  // rating ,function to print stars as a rating 
+  function rating(stars) {
  
-
-var rate =prompt ('how many star rating would you give this content, 1-5?');
-
-for(var i=0;i<rate;i++)
+for(var i=0;i<stars;i++)
 { document.write('<img src="https://www.jing.fm/clipimg/detail/1-14150_your-a-star-star-icon-png.png" width="30" height="30"/>')}
+}
+
+var userState = prompt("Are  you feel optimistic ,please type either yes or no ");
+
+checkState(userState);//invoke checkState function
+
+var userRate =prompt ('how many star rating would you give this content, 1-5?');
+document.write(`<b><p> Your rating:  </p></b> `);
+rating(userRate); //invoke rating function
